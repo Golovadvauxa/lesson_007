6feb9400602d58bc8222a8c13db5295e8deac081
Golovadvauxa <parenek.serega@gmail.com>
2022-01-12 15:00:22 +0300

    exercise '02_alchemy' done

--- 02_alchemy.py
+++ 02_alchemy.py
@@ -19,8 +19,102 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
 
+class Water:
+    def __str__(self):
+        return 'Water'
+
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Storm()
+        elif isinstance(other, Fire):
+            return Steam()
+        elif isinstance(other, Ground):
+            return Mud()
+        else:
+            return None
+
+
+class Air:
+    def __str__(self):
+        return 'Air'
+
+    def __add__(self, other):
+        if isinstance(other, Fire):
+            return Lightning()
+        elif isinstance(other, Ground):
+            return Dust()
+        elif isinstance(other, Water):
+            return Storm()
+        else:
+            return None
+
+
+class Fire:
+    def __str__(self):
+        return 'Fire'
+
+    def __add__(self, other):
+        if isinstance(other, Ground):
+            return Lava()
+        elif isinstance(other, Air):
+            return Lightning()
+        elif isinstance(other, Water):
+            return Steam()
+        else:
+            return None
+
+
+class Ground:
+    def __str__(self):
+        return 'Ground'
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Mud()
+        elif isinstance(other, Air):
+            return Dust()
+        elif isinstance(other, Fire):
+            return Lava()
+
+
+class Storm:
+    def __str__(self):
+        return 'Storm'
+
+
+class Steam:
+    def __str__(self):
+        return 'Steam'
+
+
+class Mud:
+    def __str__(self):
+        return 'Mud'
+
+
+class Lightning:
+    def __str__(self):
+        return 'Lightning'
+
+
+class Dust:
+    def __str__(self):
+        return 'Dust'
+
+
+class Lava:
+    def __str__(self):
+        return 'Lava'
+
+
+print(Water(), '+', Air(), '=', Water() + Air())
+print(Fire(), '+', Water(), '=', Fire() + Water())
+print(Water(), '+', Ground(), '=', Water() + Ground())
+print(Fire(), '+', Air(), '=', Fire() + Air())
+print(Ground(), '+', Air(), '=', Ground() + Air())
+print(Fire(), '+', Ground(), '=', Fire() + Ground())
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
 # Придумать что будет при сложении существующих элементов с новым.
+
